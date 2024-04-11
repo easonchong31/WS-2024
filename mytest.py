4e7bd3127ad5a66a5723deb0965cd526b1a7adfc
easonchong31 <91211624+easonchong31@users.noreply.github.com>
2024-04-11 12:20:10 +0100

    Add files via upload

---
+++ mytest.py
@@ -0,0 +1,18 @@
+import requests
+
+def checkServiceForWord(url, keyword):
+    try:
+        x = requests.get(url)
+        print(x.text)
+        serverStatus=1
+        if keyword in x.text:
+            print("found keyword")
+            return True
+    except:
+        print("error")
+        return False
+
+
+url = 'http://localhost:5000/getTitles'
+result = checkServiceForWord(url, 'id')
+print(result)
